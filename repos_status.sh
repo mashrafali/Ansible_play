7d17261a32e67be0285afa0ddab5d38729f5f259
Mohamed Ashraf <ceh.mohamed.ashraf@gmail.com>
2020-01-12 14:40:43 +0200

    update

--- repos_status.sh
+++ repos_status.sh
@@ -3,9 +3,14 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 REPOS_DIR=/root
 
 
+clear
 for dir in `ls -d $REPOS_DIR/*/`
 do
-  echo -e "\033[41mFor Git Repo : $dir\033[m"
+  for i in {1..10}
+  do
+    echo -n "-"
+  done
+  echo;echo -e "\033[41mFor Git Repo : $dir\033[m"
   cd $dir
   git status
   echo;echo -e "\033[36m--> Remote Pipes:\033[m"
